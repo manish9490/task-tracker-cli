01ec6368b92d9062e80c17e5653bcf55559760ce
manishsai918 <2310080057@klh.edu.in>
2026-03-02 00:10:07 +0530

    Add delete task functionality

--- task_tracker.py
+++ task_tracker.py
@@ -8,6 +8,15 @@ def list_tasks():
         status = "✓" if t["done"] else "✗"
         print(f"{i+1}. {t['task']} [{status}]")
 
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+        print("Task deleted successfully")
+    else:
+        print("Invalid task number")
+
 add_task("Learn GitHub")
 add_task("Complete assignment")
+list_tasks()
+delete_task(0)
 list_tasks()
